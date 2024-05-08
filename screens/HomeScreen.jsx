01293ddd0b4755e0409e7a45f7ce5c02a22dc716
Aniket Pratap <aniketpx@gmail.com>
2024-05-07 18:30:26 -0700

    changed filter component

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
-import { Dimensions, StyleSheet, Text, View, SafeAreaView, TouchableOpacity, TouchableWithoutFeedback, Keyboard, TextInput } from 'react-native';
-import { SelectList } from 'react-native-dropdown-select-list'
+import { Dimensions, Platform, StyleSheet, Text, View, SafeAreaView, TouchableOpacity, TouchableWithoutFeedback, Keyboard, TextInput, KeyboardAvoidingView } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
 import { useNavigation } from '@react-navigation/native'
 import { data, seasons, years } from '../components/Subjects';
 
@@ -10,8 +10,8 @@ const ClassesScreen = () => {
     const [selected, setSelected] = useState("")
     const [selectedYear, setSelectedYear] = useState("")
     const [selectedSeason, setSelectedSeason] = useState("")
-
     const [buttonPosition, setButtonPosition] = useState({ left: 0 });
+
 
     const handleYearChange = (value) => {
         if (value.length > 4) {
@@ -30,36 +30,54 @@ const ClassesScreen = () => {
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <SafeAreaView style={styles.container}>
-                <StatusBar style="auto" />
-                <View style={{ flex: 1, width: '100%', justifyContent: "space-evenly", alignItems: 'center', }}>
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                    keyboardVerticalOffset={Platform.OS === 'ios' ? 10 : 0}
+                    style={{ flex: 1, width: '100%', justifyContent: "space-evenly", alignItems: 'center', }}>
+                    <StatusBar style="auto" />
                     <Text style={{ fontSize: 45, fontWeight: 700, color: "rgba( 50, 85, 147, 100)" }}>ZotClass</Text>
-                    <SelectList
+                    <Dropdown
+                        style={styles.boxStyle}
                         data={data}
-                        boxStyles={styles.boxStyle}
-                        inputStyles={styles.inputTextStyle}
-                        dropdownTextStyles={styles.dropDownTextStyle}
-                        dropdownStyles={{ borderWidth: 0, backgroundColor: "white" }}
-                        placeholder={"Subject"}
-                        setSelected={(subject) => { setSelected(subject) }}
+                        search
+                        labelField="value"
+                        valueField="key"
+                        placeholder='Subject'
+                        placeholderStyle={{ color: "rgba(169, 169, 169, 1)", fontWeight: 700 }}
+                        searchPlaceholder="Search Subject..."
+                        selectedTextStyle={styles.selectedTextStyle}
+                        itemTextStyle={styles.dropDownTextStyle}
+                        containerStyle={{ borderRadius: 5 }}
+                        onChange={(subject) => {
+                            setSelected(subject.key);
+                        }}
                     />
-                    <SelectList
+                    <Dropdown
+                        style={styles.boxStyle}
                         data={seasons}
-                        boxStyles={styles.boxStyle}
-                        inputStyles={styles.inputTextStyle}
-                        dropdownTextStyles={styles.dropDownTextStyle}
-                        dropdownStyles={{ borderWidth: 0, backgroundColor: "white" }}
-                        placeholder={"Quarter"}
-                        search={false}
-                        maxHeight={100}
-                        setSelected={(season) => (setSelectedSeason(season))}
+                        labelField="value"
+                        valueField="key"
+                        maxHeight={200}
+                        placeholder='Quarter'
+                        placeholderStyle={{ color: "rgba(169, 169, 169, 1)", fontWeight: 700 }}
+                        selectedTextStyle={styles.selectedTextStyle}
+                        itemTextStyle={styles.dropDownTextStyle}
+                        containerStyle={{ borderRadius: 5 }}
+                        onChange={(season) => {
+                            setSelectedSeason(season.key);
+                        }}
                     />
-
                     <TextInput placeholder="2024"
                         keyboardType="numeric"
                         value={selectedYear}
                         onChangeText={handleYearChange}
                         maxLength={4}
-                        style={[styles.boxStyle, { paddingLeft: 20, paddingBottom: 10, fontWeight: "bold", color: "rgba( 50, 85, 147, 100)" }]}></TextInput>
+                        style={[styles.boxStyle, { paddingBottom: 10, fontWeight: "800", color: "rgba( 50, 85, 147, 100)" }]}
+                    >
+                    </TextInput>
+
+
+
                     <TouchableOpacity
                         style={styles.searchButton}
                         onPress={() => {
@@ -73,7 +91,8 @@ const ClassesScreen = () => {
                             <Text style={{ fontWeight: 700, color: "white" }}>Search</Text>
                         </View>
                     </TouchableOpacity>
-                </View>
+                </KeyboardAvoidingView>
+
             </SafeAreaView>
         </TouchableWithoutFeedback>
     )
@@ -96,11 +115,16 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         width: 280
     },
+    selectedTextStyle: {
+        color: "rgba( 50, 85, 147, 100)",
+        fontWeight: "bold",
+        width: 280
+    },
     boxStyle: {
+        width: "80%",
         borderRadius: 0,
         borderWidth: 0,
         borderBottomWidth: 3,
-        width: '80%',
         borderBottomColor: 'rgba( 50, 85, 147, 100)',
     },
     searchButton: {
